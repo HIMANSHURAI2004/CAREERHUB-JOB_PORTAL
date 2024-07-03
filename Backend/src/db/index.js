10a4fe710f52bfaf22eccf17be88086d8ffc7d55
HIMANSHU-RAI <raihimanshu040204@gmail.com>
2024-07-04 01:22:07 +0530

    Update MongoDB connection in db/index.js

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -6,7 +6,9 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`)
+
     } catch (error) {
+        
         console.log("MongoDB Connection Error : ",error);
         process.exit(1);
     }
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -20,25 +20,3 @@ connectDB()
 .catch((err) =>{
     console.log("MONGODB CONNECTION FAILED !!",err)
 })
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import {app} from "./app.js";
-
-
-dotenv.config({
-    path : './.env'
-})
-
-connectDB()
-.then(() => {
-    try {
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server is running at port : ${process.env.PORT}`);
-        });
-    } catch (error) {
-        console.log(`Server connection failed : `,error);
-    }
-})
-.catch((err) => {
-    console.log("MongoDB connection failed : ",err);
-})
