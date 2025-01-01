7a1f6aa29273c0a070f326fa775c47a45f7dc1cb
HIMANSHU-RAI <131878420+HIMANSHURAI2004@users.noreply.github.com>
2025-01-01 13:39:30 +0530

    add root route to respond with a greeting message

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,10 @@ app.use(cors({
     optionsSuccessStatus: 200,
 }))
 
+app.use("/", ( _ , res) => {
+  res.send("Hello from Server");
+});
+
 app.use(express.json({ limit:"5mb" }));
 app.use(express.urlencoded({extended :true,limit : "5mb"}));
 app.use(express.static("public"));
